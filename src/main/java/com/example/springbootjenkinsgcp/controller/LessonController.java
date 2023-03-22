0615d9dbbfacc82a5979362d156365d3a63b5bac
rasitesdmr <mrasitesdemir7@gmail.com>
2023-03-22 23:29:38 +0300

    Updated

--- src/main/java/com/example/springbootjenkinsgcp/controller/LessonController.java
+++ src/main/java/com/example/springbootjenkinsgcp/controller/LessonController.java
@@ -3,6 +3,7 @@ package com.example.springbootjenkinsgcp.controller;
 import com.example.springbootjenkinsgcp.dto.request.LessonCreateRequest;
 import com.example.springbootjenkinsgcp.dto.response.LessonCreateResponse;
 import com.example.springbootjenkinsgcp.service.LessonService;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,5 +22,14 @@ public class LessonController {
     ResponseEntity<LessonCreateResponse> createLesson(LessonCreateRequest lessonCreateRequest){
         return new ResponseEntity<>(lessonService.createLesson(lessonCreateRequest), HttpStatus.CREATED);
     }
+    @PostMapping("/createUser")
+    public String createUser(@RequestHeader("email") @Parameter(hidden = true) String email) {
+        return "aaaaaaaaa";
+    }
 
+
+    @PostMapping("/createUser")
+    public String  createUsersssssss(@RequestHeader("email")String email) {
+       return "vvvvvvvvvvvvvvvv";
+    }
 }
